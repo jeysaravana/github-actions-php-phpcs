ecba44edd7d1f6355aea52c75d2e9f4e787ab1cd
Jey Saravana <jey.saravana@gmail.com>
2020-09-11 10:35:00 -0400

    test

--- src/Greetings.php
+++ src/Greetings.php
@@ -3,9 +3,8 @@
 namespace Main;
 
 class Greetings {
-    public static function sayHelloWorld()
-    {
-		$arr = array( 'one' => 1, 'two' => 2 );
+    public static function sayHelloWorld() {
+	 $arr = array( 'one' => 1, 'two' => 2 );
         return 'Hello World';
     }
 }
